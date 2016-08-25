efe080388b94a6917aaf73c7560a7320ce13881c
Jamis Buck <jamisbuck@gmail.com>
2016-08-25 12:16:01 -0600

    lines w/ thickness
    definitely needs work to remove duplicate code, but it WORKS

--- 004-draw-lines/src/image/mod.rs
+++ 004-draw-lines/src/image/mod.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 pub struct Image {
   width: usize,
   height: usize,
@@ -20,14 +22,20 @@ impl Image {
 
   pub fn put_pixel(&mut self, x: usize, y: usize, color: u32)
   {
-    let index = y * self.width + x;
-    self.pixels[index] = color;
+    if x < self.width && y < self.height {
+      let index = y * self.width + x;
+      self.pixels[index] = color;
+    }
   }
 
   pub fn get_pixel(&self, x: usize, y: usize) -> u32
   {
-    let index = y * self.width + x;
-    self.pixels[index]
+    if x < self.width && y < self.height {
+      let index = y * self.width + x;
+      self.pixels[index]
+    } else {
+      0
+    }
   }
 
   fn preferred_grade((x1, y1): (usize, usize), (x2, y2): (usize, usize)) -> (bool, (usize, usize), (usize, usize))
@@ -83,7 +91,270 @@ impl Image {
     }
   }
 
-  pub fn to_ppm(&self) -> String
+  // adapted from the discussion and code presented here:
+  // http://kt8216.unixcab.org/murphy/index.html
+  pub fn draw_thick_line<F>(&mut self,
+                            p1: (usize, usize),
+                            p2: (usize, usize),
+                            color: u32,
+                            left_width: F,
+                            right_width: F)
+    where F: Fn(isize, isize) -> isize
+  {
+    let (dx, xstep) = if p1.0 > p2.0 {
+        (p1.0 as isize - p2.0 as isize, -1)
+      } else if p1.0 == p2.0 {
+        (0, 0)
+      } else {
+        (p2.0 as isize - p1.0 as isize, 1)
+      };
+
+    let (dy, ystep) = if p1.1 > p2.1 {
+        (p1.1 as isize - p2.1 as isize, -1)
+      } else if p1.1 == p2.1 {
+        (0, 0)
+      } else {
+        (p2.1 as isize - p1.1 as isize, 1)
+      };
+
+    // compute the perpendicular x/y steps
+    let (pystep, pxstep, left_fn, right_fn) = match xstep + ystep*4 {
+      -5 => (-1,  1, right_width, left_width),  // -1 + -1*4
+      -1 => (-1,  0, right_width, left_width),  // -1 +  0*4
+       3 => ( 1,  1, left_width,  right_width), // -1 +  1*4
+      -4 => ( 0, -1, left_width,  right_width), //  0 + -1*4
+       0 => ( 0,  0, left_width,  right_width), //  0 +  0*4
+       4 => ( 0,  1, left_width,  right_width), //  0 +  1*4
+      -3 => (-1, -1, left_width,  right_width), //  1 + -1*4
+       1 => (-1,  0, left_width,  right_width), //  1 +  0*4
+       5 => ( 1, -1, right_width, left_width),  //  1 +  1*4
+       _ => ( 9,  9, left_width,  right_width),
+    };
+
+    if dx > dy {
+      self.x_varthick_line(p1, color, dx, dy,
+                           xstep, ystep,
+                           left_fn, right_fn,
+                           pxstep, pystep);
+    } else {
+      self.y_varthick_line(p1, color, dx, dy,
+                           xstep, ystep,
+                           left_fn, right_fn,
+                           pxstep, pystep);
+    }
+  }
+
+  fn x_perpendicular(&mut self,
+                     p1: (isize, isize),
+                     color: u32,
+                     dx: isize, dy: isize,
+                     xstep: isize, ystep: isize,
+                     einit: isize,
+                     w_left: isize, w_right: isize,
+                     winit: isize)
+  {
+    let threshold = dx - 2 * dy;
+    let e_diag = -2 * dx;
+    let e_square = 2 * dy;
+    let mut p: isize = 0;
+    let mut q: isize = 0;
+    let mut x = p1.0;
+    let mut y = p1.1;
+    let mut error = einit;
+    let mut tk = dx + dy - winit;
+
+    while tk <= w_left {
+      self.put_pixel(x as usize, y as usize, color);
+
+      if error >= threshold {
+        x += xstep;
+        error += e_diag;
+        tk += 2 * dy;
+      }
+
+      error += e_square;
+      y += ystep;
+      tk += 2 * dx;
+      q += 1;
+    }
+
+    x = p1.0;
+    y = p1.1;
+    error = -einit;
+    tk = dx + dy + winit;
+
+    while tk <= w_right {
+      if p > 0 { self.put_pixel(x as usize, y as usize, color); }
+
+      if error > threshold {
+        x -= xstep;
+        error += e_diag;
+        tk += 2 * dy;
+      }
+
+      error += e_square;
+      y -= ystep;
+      tk += 2 * dx;
+      p += 1;
+    }
+
+    // for very thin lines
+    if q == 0 && p < 2 {
+      self.put_pixel(p1.0 as usize, p1.1 as usize, color);
+    }
+  }
+
+  fn x_varthick_line<F>(&mut self,
+                        p1: (usize, usize),
+                        color: u32,
+                        dx: isize, dy: isize,
+                        xstep: isize, ystep: isize,
+                        left_width: F,
+                        right_width: F,
+                        pxstep: isize, pystep: isize)
+    where F: Fn(isize, isize) -> isize
+  {
+    let mut p_error: isize = 0;
+    let mut error: isize = 0;
+    let mut x = p1.0 as isize;
+    let mut y = p1.1 as isize;
+    let threshold = dx - 2 * dy;
+    let e_diag = -2 * dx;
+    let e_square = 2 * dy;
+    let length = dx + 1;
+    let d = (( dx * dx + dy * dy ) as f64).sqrt();
+
+    for p in 0..length {
+      let w_left = ((left_width(p, length) * 2) as f64 * d) as isize;
+      let w_right = ((right_width(p, length) * 2) as f64 * d) as isize;
+      self.x_perpendicular((x, y), color, dx, dy, pxstep, pystep,
+                           p_error, w_left, w_right, error);
+
+      if error >= threshold {
+        y += ystep;
+        error += e_diag;
+
+        if p_error >= threshold {
+          self.x_perpendicular((x, y), color, dx, dy, pxstep, pystep,
+                               (p_error+e_diag+e_square),
+                               w_left, w_right, error);
+          p_error += e_diag;
+        }
+
+        p_error += e_square;
+      }
+
+      error += e_square;
+      x += xstep;
+    }
+  }
+
+  fn y_perpendicular(&mut self,
+                     p1: (isize, isize),
+                     color: u32,
+                     dx: isize, dy: isize,
+                     xstep: isize, ystep: isize,
+                     einit: isize,
+                     w_left: isize, w_right: isize,
+                     winit: isize)
+  {
+    let threshold = dy - 2 * dx;
+    let e_diag = -2 * dy;
+    let e_square = 2 * dx;
+    let mut p: isize = 0;
+    let mut q: isize = 0;
+    let mut x = p1.0;
+    let mut y = p1.1;
+    let mut error = -einit;
+    let mut tk = dx + dy + winit;
+
+    while tk <= w_left {
+      self.put_pixel(x as usize, y as usize, color);
+
+      if error > threshold {
+        y += ystep;
+        error += e_diag;
+        tk += 2 * dx;
+      }
+
+      error += e_square;
+      x += xstep;
+      tk += 2 * dy;
+      q += 1;
+    }
+
+    x = p1.0;
+    y = p1.1;
+    error = einit;
+    tk = dx + dy - winit;
+
+    while tk <= w_right {
+      if p > 0 { self.put_pixel(x as usize, y as usize, color); }
+
+      if error >= threshold {
+        y -= ystep;
+        error += e_diag;
+        tk += 2 * dx;
+      }
+
+      error += e_square;
+      x -= xstep;
+      tk += 2 * dy;
+      p += 1;
+    }
+
+    // for very thin lines
+    if q == 0 && p < 2 {
+      self.put_pixel(p1.0 as usize, p1.1 as usize, color);
+    }
+  }
+
+  fn y_varthick_line<F>(&mut self,
+                        p1: (usize, usize),
+                        color: u32,
+                        dx: isize, dy: isize,
+                        xstep: isize, ystep: isize,
+                        left_width: F,
+                        right_width: F,
+                        pxstep: isize, pystep: isize)
+    where F: Fn(isize, isize) -> isize
+  {
+    let mut p_error: isize = 0;
+    let mut error: isize = 0;
+    let mut x = p1.0 as isize;
+    let mut y = p1.1 as isize;
+    let threshold = dy - 2 * dx;
+    let e_diag = -2 * dy;
+    let e_square = 2 * dx;
+    let length = dy + 1;
+    let d = (( dx * dx + dy * dy ) as f64).sqrt();
+
+    for p in 0..length {
+      let w_left = ((left_width(p, length) * 2) as f64 * d) as isize;
+      let w_right = ((right_width(p, length) * 2) as f64 * d) as isize;
+      self.y_perpendicular((x, y), color, dx, dy, pxstep, pystep,
+                           p_error, w_left, w_right, error);
+
+      if error >= threshold {
+        x += xstep;
+        error += e_diag;
+
+        if p_error >= threshold {
+          self.y_perpendicular((x, y), color, dx, dy, pxstep, pystep,
+                               (p_error+e_diag+e_square),
+                               w_left, w_right, error);
+          p_error += e_diag;
+        }
+
+        p_error += e_square;
+      }
+
+      error += e_square;
+      y += ystep;
+    }
+  }
+
+  pub fn to_ppm<I: Write>(&self, mut out: I)
   {
     let mut buffer = String::from("P3\n");
     let header = format!("{} {}\n255\n", self.width, self.height);
@@ -101,6 +372,9 @@ impl Image {
       buffer.push('\n');
     }
 
-    buffer
+    let result = out.write_all(buffer.as_bytes());
+    if !result.is_ok() {
+      println!("could not write image to file");
+    }
   }
 }
--- 004-draw-lines/src/main.rs
+++ 004-draw-lines/src/main.rs
@@ -1,22 +1,38 @@
 extern crate rand;
 
 mod image;
+use std::fs::File;
+use std::error::Error;
 
 fn main() {
   let mut image = image::Image::new(503, 503, 0xffffff);
   let (width, height) = image.dimensions();
 
+  let line_width = |p: isize, length: isize| 6 as isize;
+
   let mut x = 1;
   while x < width-1 {
-    let color = rand::random::<u32>();
+    //let color = rand::random::<u32>();
+    let intensity = (255 * (x - 1) / (width - 2)) as u32;
+    let color = 0xff0000 +
+                (intensity << 8) +
+                intensity;
 
-    image.draw_line((x,1), (width-2,x), color);
-    image.draw_line((width-2,x), (width-x,height-2), color);
-    image.draw_line((width-x,height-2), (1,width-x), color);
-    image.draw_line((1,width-x), (x,1), color);
+    //image.draw_line((x,1), (width-2,x), color);
+    //image.draw_line((width-2,x), (width-x,height-2), color);
+    //image.draw_line((width-x,height-2), (1,width-x), color);
+    //image.draw_line((1,width-x), (x,1), color);
+
+    image.draw_thick_line((x,1), (width-2,x), color, &line_width, &line_width);
+    image.draw_thick_line((width-2,x), (width-x,height-2), color, &line_width, &line_width);
+    image.draw_thick_line((width-x,height-2), (1,width-x), color, &line_width, &line_width);
+    image.draw_thick_line((1,width-x), (x,1), color, &line_width, &line_width);
+
     x += 10;
   }
 
-  let ppm = image.to_ppm();
-  println!("{}", ppm);
+  match File::create("lines.ppm") {
+    Err(why) => println!("couldn't create lines.ppm ({})", why.description()),
+    Ok(file) => image.to_ppm(file)
+  };
 }
