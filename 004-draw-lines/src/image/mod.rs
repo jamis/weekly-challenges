4024036fec2cf94df775d5a68421b1a01b9d97d5
Jamis Buck <jamisbuck@gmail.com>
2016-08-25 22:47:09 -0600

    dry things up

--- 004-draw-lines/src/image/mod.rs
+++ 004-draw-lines/src/image/mod.rs
@@ -131,124 +131,44 @@ impl Image {
       };
 
     // compute the perpendicular x/y steps
-    let (pystep, pxstep, left_fn, right_fn) = match xstep + ystep*4 {
-      -5 => (-1,  1, right_width, left_width),  // -1 + -1*4
-      -1 => (-1,  0, right_width, left_width),  // -1 +  0*4
-       3 => ( 1,  1, left_width,  right_width), // -1 +  1*4
-      -4 => ( 0, -1, left_width,  right_width), //  0 + -1*4
-       0 => ( 0,  0, left_width,  right_width), //  0 +  0*4
-       4 => ( 0,  1, left_width,  right_width), //  0 +  1*4
-      -3 => (-1, -1, left_width,  right_width), //  1 + -1*4
-       1 => (-1,  0, left_width,  right_width), //  1 +  0*4
-       5 => ( 1, -1, right_width, left_width),  //  1 +  1*4
-       _ => ( 9,  9, left_width,  right_width),
+    let (pystep, pxstep, left_fn, right_fn) = match (xstep, ystep) {
+      (-1, -1) => (-1,  1, right_width, left_width),
+      (-1,  0) => (-1,  0, right_width, left_width),
+      (-1,  1) => ( 1,  1, left_width,  right_width),
+      ( 0, -1) => ( 0, -1, left_width,  right_width),
+      ( 0,  0) => ( 0,  0, left_width,  right_width),
+      ( 0,  1) => ( 0,  1, left_width,  right_width),
+      ( 1, -1) => (-1, -1, left_width,  right_width),
+      ( 1,  0) => (-1,  0, left_width,  right_width),
+      ( 1,  1) => ( 1, -1, right_width, left_width),
+      _        => ( 9,  9, left_width,  right_width),
     };
 
-    if dx > dy {
-      self.x_varthick_line(p1, color, dx, dy,
-                           xstep, ystep,
-                           left_fn, right_fn,
-                           pxstep, pystep);
-    } else {
-      self.y_varthick_line(p1, color, dx, dy,
-                           xstep, ystep,
-                           left_fn, right_fn,
-                           pxstep, pystep);
-    }
-  }
+    let swapped = dy >= dx;
+    let (da, db) = if swapped { (dy, dx) } else { (dx, dy) };
+    let threshold = da - 2 * db;
+    let e_diag = -2 * da;
+    let e_square = 2 * db;
+    let length = da + 1;
+    let d = (( dx * dx + dy * dy ) as f64).sqrt();
 
-  fn x_perpendicular(&mut self,
-                     p1: (isize, isize),
-                     color: u32,
-                     dx: isize, dy: isize,
-                     xstep: isize, ystep: isize,
-                     einit: isize,
-                     w_left: isize, w_right: isize,
-                     winit: isize)
-  {
-    let threshold = dx - 2 * dy;
-    let e_diag = -2 * dx;
-    let e_square = 2 * dy;
-    let mut p: isize = 0;
-    let mut q: isize = 0;
-    let mut x = p1.0;
-    let mut y = p1.1;
-    let mut error = einit;
-    let mut tk = dx + dy - winit;
-
-    while tk <= w_left {
-      self.put_pixel(x as usize, y as usize, color);
-
-      if error >= threshold {
-        x += xstep;
-        error += e_diag;
-        tk += 2 * dy;
-      }
-
-      error += e_square;
-      y += ystep;
-      tk += 2 * dx;
-      q += 1;
-    }
-
-    x = p1.0;
-    y = p1.1;
-    error = -einit;
-    tk = dx + dy + winit;
-
-    while tk <= w_right {
-      if p > 0 { self.put_pixel(x as usize, y as usize, color); }
-
-      if error > threshold {
-        x -= xstep;
-        error += e_diag;
-        tk += 2 * dy;
-      }
-
-      error += e_square;
-      y -= ystep;
-      tk += 2 * dx;
-      p += 1;
-    }
-
-    // for very thin lines
-    if q == 0 && p < 2 {
-      self.put_pixel(p1.0 as usize, p1.1 as usize, color);
-    }
-  }
-
-  fn x_varthick_line<F>(&mut self,
-                        p1: (usize, usize),
-                        color: u32,
-                        dx: isize, dy: isize,
-                        xstep: isize, ystep: isize,
-                        left_width: F,
-                        right_width: F,
-                        pxstep: isize, pystep: isize)
-    where F: Fn(isize, isize) -> isize
-  {
     let mut p_error: isize = 0;
     let mut error: isize = 0;
     let mut x = p1.0 as isize;
     let mut y = p1.1 as isize;
-    let threshold = dx - 2 * dy;
-    let e_diag = -2 * dx;
-    let e_square = 2 * dy;
-    let length = dx + 1;
-    let d = (( dx * dx + dy * dy ) as f64).sqrt();
 
     for p in 0..length {
-      let w_left = ((left_width(p, length) * 2) as f64 * d) as isize;
-      let w_right = ((right_width(p, length) * 2) as f64 * d) as isize;
-      self.x_perpendicular((x, y), color, dx, dy, pxstep, pystep,
+      let w_left = ((left_fn(p, length) * 2) as f64 * d) as isize;
+      let w_right = ((right_fn(p, length) * 2) as f64 * d) as isize;
+      self.perpendicular((x, y), color, dx, dy, pxstep, pystep,
                            p_error, w_left, w_right, error);
 
       if error >= threshold {
-        y += ystep;
+        if swapped { x += xstep } else { y += ystep }
         error += e_diag;
 
         if p_error >= threshold {
-          self.x_perpendicular((x, y), color, dx, dy, pxstep, pystep,
+          self.perpendicular((x, y), color, dx, dy, pxstep, pystep,
                                (p_error+e_diag+e_square),
                                w_left, w_right, error);
           p_error += e_diag;
@@ -258,112 +178,69 @@ impl Image {
       }
 
       error += e_square;
-      x += xstep;
+      if swapped { y += ystep } else { x += xstep };
     }
   }
 
-  fn y_perpendicular(&mut self,
-                     p1: (isize, isize),
-                     color: u32,
-                     dx: isize, dy: isize,
-                     xstep: isize, ystep: isize,
-                     einit: isize,
-                     w_left: isize, w_right: isize,
-                     winit: isize)
+  fn perpendicular(&mut self,
+                   p1: (isize, isize),
+                   color: u32,
+                   dx: isize, dy: isize,
+                   xstep: isize, ystep: isize,
+                   einit: isize,
+                   w_left: isize, w_right: isize,
+                   winit: isize)
   {
-    let threshold = dy - 2 * dx;
-    let e_diag = -2 * dy;
-    let e_square = 2 * dx;
+    let swapped = dy >= dx;
+    let (da, db, sign) = if swapped { (dy, dx, -1) } else { (dx, dy, 1) };
+    let threshold = da - 2 * db;
+    let e_diag = -2 * da;
+    let e_square = 2 * db;
     let mut p: isize = 0;
     let mut q: isize = 0;
     let mut x = p1.0;
     let mut y = p1.1;
-    let mut error = -einit;
-    let mut tk = dx + dy + winit;
+    let mut error = sign * einit;
+    let mut tk = dx + dy - sign * winit;
 
     while tk <= w_left {
       self.put_pixel(x as usize, y as usize, color);
 
-      if error > threshold {
-        y += ystep;
+      if error >= threshold {
+        if swapped { y += ystep } else { x += xstep };
         error += e_diag;
-        tk += 2 * dx;
+        tk += 2 * db;
       }
 
       error += e_square;
-      x += xstep;
-      tk += 2 * dy;
+      if swapped { x += xstep } else { y += ystep };
+      tk += 2 * da;
       q += 1;
     }
 
     x = p1.0;
     y = p1.1;
-    error = einit;
-    tk = dx + dy - winit;
+    error = -sign * einit;
+    tk = dx + dy + sign * winit;
 
     while tk <= w_right {
       if p > 0 { self.put_pixel(x as usize, y as usize, color); }
 
-      if error >= threshold {
-        y -= ystep;
+      if error > threshold {
+        if swapped { y -= ystep } else { x -= xstep }
         error += e_diag;
-        tk += 2 * dx;
+        tk += 2 * db;
       }
 
       error += e_square;
-      x -= xstep;
-      tk += 2 * dy;
+      if swapped { x -= xstep } else { y -= ystep };
+      tk += 2 * da;
       p += 1;
     }
 
     // for very thin lines
     if q == 0 && p < 2 {
       self.put_pixel(p1.0 as usize, p1.1 as usize, color);
-    }
-  }
-
-  fn y_varthick_line<F>(&mut self,
-                        p1: (usize, usize),
-                        color: u32,
-                        dx: isize, dy: isize,
-                        xstep: isize, ystep: isize,
-                        left_width: F,
-                        right_width: F,
-                        pxstep: isize, pystep: isize)
-    where F: Fn(isize, isize) -> isize
-  {
-    let mut p_error: isize = 0;
-    let mut error: isize = 0;
-    let mut x = p1.0 as isize;
-    let mut y = p1.1 as isize;
-    let threshold = dy - 2 * dx;
-    let e_diag = -2 * dy;
-    let e_square = 2 * dx;
-    let length = dy + 1;
-    let d = (( dx * dx + dy * dy ) as f64).sqrt();
-
-    for p in 0..length {
-      let w_left = ((left_width(p, length) * 2) as f64 * d) as isize;
-      let w_right = ((right_width(p, length) * 2) as f64 * d) as isize;
-      self.y_perpendicular((x, y), color, dx, dy, pxstep, pystep,
-                           p_error, w_left, w_right, error);
-
-      if error >= threshold {
-        x += xstep;
-        error += e_diag;
-
-        if p_error >= threshold {
-          self.y_perpendicular((x, y), color, dx, dy, pxstep, pystep,
-                               (p_error+e_diag+e_square),
-                               w_left, w_right, error);
-          p_error += e_diag;
-        }
-
-        p_error += e_square;
-      }
-
-      error += e_square;
-      y += ystep;
     }
   }
 
