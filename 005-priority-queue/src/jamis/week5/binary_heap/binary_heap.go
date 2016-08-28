adc96f6726400cbedcd04e62ba8001537c12ee23
Jamis Buck <jamisbuck@gmail.com>
2016-08-28 09:25:55 -0600

    hey, it actually works!

--- 005-priority-queue/src/jamis/week5/binary_heap/binary_heap.go
+++ 005-priority-queue/src/jamis/week5/binary_heap/binary_heap.go
@@ -1,22 +1,77 @@
 package binary_heap
 
 type Node struct {
-  parent, left, right *Node
-  value *interface{}
+  value interface{}
   priority int
 }
 
 type Heap struct {
-  root *Node
-  higher func (interface{}, interface{}) bool
+  list []Node
+  higher func (int, int) bool
 }
 
-func NewHeap(higher func (interface{}, interface{}) bool) *Heap {
+func NewHeap(higher func (int, int) bool) *Heap {
   heap := new(Heap)
+  heap.list = make([]Node, 0, 10)
   heap.higher = higher
   return heap
 }
 
 func (heap *Heap) IsEmpty() bool {
-  return heap.root == nil
+  return len(heap.list) == 0
+}
+
+func (heap *Heap) Insert(value interface{}, priority int) {
+  heap.list = append(heap.list, Node { value, priority })
+  heap.upHeapFrom(len(heap.list)-1)
+}
+
+func (heap *Heap) Extract() (interface{}, int) {
+  if len(heap.list) == 0 {
+    return nil, 0
+  } else {
+    root := heap.list[0]
+    heap.list[0] = heap.list[len(heap.list)-1]
+    heap.list = heap.list[:len(heap.list)-1]
+    heap.downHeapFrom(0)
+    return root.value, root.priority
+  }
+}
+
+func (heap *Heap) Peek() (interface{}, int) {
+  if len(heap.list) > 0 {
+    return heap.list[0].value, heap.list[0].priority
+  } else {
+    return nil, 0
+  }
+}
+
+func (heap *Heap) upHeapFrom(child int) {
+  if child > 0 {
+    parent := (child - 1) >> 1
+
+    if heap.higher(heap.list[child].priority, heap.list[parent].priority) {
+      heap.list[child], heap.list[parent] = heap.list[parent], heap.list[child]
+      heap.upHeapFrom(parent)
+    }
+  }
+}
+
+func (heap *Heap) downHeapFrom(root int) {
+  left := root * 2 + 1
+  right := root * 2 + 2
+  preferred_root := root
+
+  if left < len(heap.list) && heap.higher(heap.list[left].priority, heap.list[preferred_root].priority) {
+    preferred_root = left
+  }
+
+  if right < len(heap.list) && heap.higher(heap.list[right].priority, heap.list[preferred_root].priority) {
+    preferred_root = right
+  }
+
+  if preferred_root != root {
+    heap.list[preferred_root], heap.list[root] = heap.list[root], heap.list[preferred_root]
+    heap.downHeapFrom(preferred_root)
+  }
 }
--- 005-priority-queue/src/jamis/week5/main.go
+++ 005-priority-queue/src/jamis/week5/main.go
@@ -2,18 +2,22 @@ package main
 
 import (
   "fmt"
+  "math/rand"
   "jamis/week5/binary_heap"
 )
 
-func compareInt(a interface{}, b interface{}) bool {
-  i1 := a.(int)
-  i2 := b.(int)
-
-  return i1 > i2
+func compareInt(a int, b int) bool {
+  return a > b
 }
 
 func main() {
   heap := binary_heap.NewHeap(compareInt)
 
-  fmt.Println(heap.IsEmpty())
+  for i := 0; i < 100; i++ {
+    heap.Insert("a", rand.Intn(1000))
+  }
+
+  for !heap.IsEmpty() {
+    fmt.Println(heap.Extract())
+  }
 }
