590e4f30a691874dc849180fac8cb5b095ad2b48
Jamis Buck <jamisbuck@gmail.com>
2016-09-18 10:51:50 -0600

    initial Haskell implementation of calculator

---
+++ 008-calculator/README.md
@@ -0,0 +1,19 @@
+# Weekly Challenge #8: Simple Parser and Interpreter
+
+http://weblog.jamisbuck.org/2016/9/17/weekly-programming-challenge-8.html
+
+Here, we get to implement a parser and interpreter for a simple calculator
+grammar. I'm going to be brave and give it a go in Haskell. Will I succeed?
+Stay tuned!
+
+## LICENSE
+
+This code is provided as-is, with no guarantees or promises of support of
+any kind, implied or otherwise. You may use it however you wish, the sole
+exception that you may not claim authorship or any kind of financial control
+over the code.
+
+
+## AUTHOR
+
+Jamis Buck <jamis@jamisbuck.org>
---
+++ 008-calculator/src/Calc.hs
@@ -0,0 +1,73 @@
+import System.Environment
+import Data.Char
+
+data Token = LParen
+           | RParen
+           | Plus
+           | Minus
+           | Times
+           | Divide
+           | Number Int deriving (Show)
+
+data AST = Unary Token AST
+         | Binary AST Token AST
+         | Literal Int deriving (Show)
+
+type State = (AST, [Token])
+
+tokenize :: String -> [Token]
+tokenize "" = []
+tokenize (' ':cs) = tokenize cs
+tokenize ('(':cs) = LParen:tokenize cs
+tokenize (')':cs) = RParen:tokenize cs
+tokenize ('+':cs) = Plus:tokenize cs
+tokenize ('-':cs) = Minus:tokenize cs
+tokenize ('*':cs) = Times:tokenize cs
+tokenize ('/':cs) = Divide:tokenize cs
+tokenize (c:cs)
+  | isDigit c = (Number $ digitToInt c):tokenize cs
+  | otherwise = error [c]
+
+parse :: [Token] -> AST
+parse tokens =
+  case expression tokens of (ast, []) -> ast
+                            (ast, ts) -> error "trailing tokens"
+
+binary :: AST -> Token -> State -> State
+binary left op (right, tokens) = (Binary left op right, tokens)
+
+expression :: [Token] -> State
+expression tokens =
+  case term tokens of
+    (left, (Plus:tokens))  -> binary left Plus (expression tokens)
+    (left, (Minus:tokens)) -> binary left Minus (expression tokens)
+    state                  -> state
+
+term :: [Token] -> State
+term tokens =
+  case factor tokens of
+    (left, (Times:tokens))  -> binary left Times (term tokens)
+    (left, (Divide:tokens)) -> binary left Divide (term tokens)
+    state                   -> state
+
+factor :: [Token] -> State
+factor (Minus:tokens) =
+  let (value, tokens') = factor tokens
+  in (Unary Minus value, tokens')
+factor (LParen:tokens) =
+  let (ast, tokens') = expression tokens
+      (RParen:tokens'') = tokens'
+  in (ast, tokens'')
+factor ((Number n):tokens) = (Literal n, tokens)
+factor tokens = error "invalid token"
+
+evaluate :: (Fractional a) => AST -> a
+evaluate (Binary left Plus right) = (evaluate left) + (evaluate right)
+evaluate (Binary left Minus right) = (evaluate left) - (evaluate right)
+evaluate (Binary left Times right) = (evaluate left) * (evaluate right)
+evaluate (Binary left Divide right) = (evaluate left) / (evaluate right)
+evaluate (Unary Minus expr) = - (evaluate expr)
+evaluate (Literal n) = fromIntegral(n)
+
+main :: IO ()
+main = getArgs >>= print . evaluate . parse . tokenize . head
